7358b6bebe80c601ae3b51190616ddfb2eb396d0
JP <jpyepezimc@gmail.com>
2018-10-04 11:48:08 +1300

    added social tags

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const SocialTags = require('social-tags-webpack-plugin');
 
 module.exports = {
     entry: ['babel-polyfill', './src/js/index.js' ],
@@ -14,6 +15,26 @@ module.exports = {
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './src/index.html'
+        }),
+        new SocialTags({
+            appUrl: 'https://projects.jpyepez.com/EmojiP5/',
+      facebook: {
+        'og:url': "https://projects.jpyepez.com/EmojiP5/",
+        'og:type': "website",
+        'og:title': "Top 100 Emoji on Twitter",
+        'og:image': './src/assets/thumb.png',
+        'og:description': "Creative visualization of real-time emoji use on Twitter.",
+        'og:site_name': "JP Yepez - Projects",
+        'og:locale': "en_US",
+      },
+      twitter: {
+        "twitter:card": "summary",
+        "twitter:creator": "@jpyepezartist",
+        "twitter:url": "https://projects.jpyepez.com/EmojiP5/",
+        "twitter:title": "Top 100 Emoji on Twitter",
+        "twitter:description": "Creative visualization of real-time emoji use on Twitter.",
+        "twitter:image": './src/assets/thumb.png'
+      }
         })
     ],
     module: {
